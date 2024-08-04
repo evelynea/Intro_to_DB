1dcd0c486f75480118ae5186f068a40b5a47b5a8
evelynea <93907954+evelynea@users.noreply.github.com>
2024-08-04 02:08:45 +0200

    Update task_2.sql

--- task_2.sql
+++ task_2.sql
@@ -1,10 +1,9 @@
--- Use the alx_book_store database
 USE alx_book_store;
 
 -- Create the 'Authors' table
 CREATE TABLE IF NOT EXISTS Authors (
-    AuthorID INT AUTO_INCREMENT PRIMARY KEY,
-    AuthorName VARCHAR(255) NOT NULL,
+    Author_id INT AUTO_INCREMENT PRIMARY KEY,
+    Author_name VARCHAR(255) NOT NULL,
     Bio TEXT
 );
 
@@ -22,7 +21,31 @@ CREATE TABLE IF NOT EXISTS Books (
 -- Create the 'Customers' table
 CREATE TABLE IF NOT EXISTS Customers (
     CustomerID INT AUTO_INCREMENT PRIMARY KEY,
-    CustomerName VARCHAR(255) NOT NULL,
+    CustomerName VARCHAR(215) NOT NULL,
+    Email VARCHAR(215) UNIQUE NOT NULL,
+    Phone VARCHAR(15),
+    Address TEXT
+);
+
+-- Create the 'Orders' table
+CREATE TABLE IF NOT EXISTS Orders (
+    ID INT AUTO_INCREMENT PRIMARY KEY,
+    CustomerID INT,
+    OrderDate DATE NOT NULL,
+    FOREIGN KEY (CustomerID) REFERENCES Customers(CustomerID)
+);
+
+-- Create the 'OrderDetails' table
+CREATE TABLE IF NOT EXISTS OrderDetails (
+    ID INT AUTO_INCREMENT PRIMARY KEY,
+    OrderID INT,
+    BookID INT,
+    Quantity INT NOT NULL,
+    Price DECIMAL(10, 2) NOT NULL,
+    FOREIGN KEY (OrderID) REFERENCES Orders(ID),
+    FOREIGN KEY (BookID) REFERENCES Books(ID)
+);
+) NOT NULL,
     Email VARCHAR(255) UNIQUE NOT NULL,
     Phone VARCHAR(15),
     Address TEXT
