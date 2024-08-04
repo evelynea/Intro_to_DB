61df4cd0bd01adf6bc3dc5837888bfc18b54df36
evelynea <93907954+evelynea@users.noreply.github.com>
2024-08-04 02:20:58 +0200

    Create task_4.sql

---
+++ task_4.sql
@@ -0,0 +1,16 @@
+USE alx_book_store;
+
+-- Query the information_schema to get the table structure
+SELECT 
+    COLUMN_NAME AS 'Column Name',
+    COLUMN_TYPE AS 'Data Type',
+    IS_NULLABLE AS 'Nullable',
+    COLUMN_DEFAULT AS 'Default Value',
+    EXTRA AS 'Extra Info'
+FROM 
+    information_schema.COLUMNS
+WHERE 
+    TABLE_SCHEMA = 'alx_book_store'
+    AND TABLE_NAME = 'Books'
+ORDER BY 
+    ORDINAL_POSITION;
